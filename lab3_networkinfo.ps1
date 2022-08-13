88c128abce95390fb70815454d81d6f9d708681b
200472192 <111205707+200472192@users.noreply.github.com>
2022-08-13 19:04:40 -0400

    Create lab3_networkinfo.ps1

---
+++ lab3_networkinfo.ps1
@@ -0,0 +1,5 @@
+﻿Get-ciminstance -className Win32_networkadapterconfiguration |
+Where-Object { 
+$_.ipenabled -eq "true"
+} | 
+ft -a Description, Index, IPAddress, IPSubnet, DNSDomain, DNSServersearchorder
